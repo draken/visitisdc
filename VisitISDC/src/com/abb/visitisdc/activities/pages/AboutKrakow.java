a743f4a92b54c363e8b4464b75079c94820b904e
mnowrot <marcin.nowrot@gmail.com>
2013-06-10 00:46:52 +0200

    About Krakow tab ready

--- VisitISDC/src/com/abb/visitisdc/activities/pages/AboutKrakow.java
+++ VisitISDC/src/com/abb/visitisdc/activities/pages/AboutKrakow.java
@@ -3,8 +3,12 @@
  */
 package com.abb.visitisdc.activities.pages;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
+import com.abb.visitisdc.R;
 import com.abb.visitisdc.activities.PageActivity;
 import com.abb.visitisdc.fragments.pages.FragmentAboutKrakow;
 
@@ -17,5 +21,11 @@ public class AboutKrakow extends PageActivity {
 	@Override
 	protected Fragment getContentFragment() {
 		return new FragmentAboutKrakow();
+	}
+
+	public void onClickGoToKrakowWebsite(View v) {
+		Uri uri = Uri.parse(getResources().getString(R.string.about_krakow_website_link));
+		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(intent);
 	}
 }
